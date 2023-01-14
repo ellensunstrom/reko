dc5969b613f13a970e2941030c701a9d74e42f27
Ellen Sunström Thörnberg <ellen.sunstrom@gmail.com>
2023-01-14 16:14:14 +0100

    Testing classes

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
 # first try
 
+
+class Recipe:
+    def __init__(self, title, description, ingredients, instructions):
+      self.title = title
+      self.description = description
+      self.ingredients = ingredients
+      self.instructions = instructions
+
+class RecipeManager:
+    def __init__(self):
+       pass
+
 print("Hello world")
+spags = Recipe("spago", "", "2 spags", "boil spag")
+print(spags.ingredients)
+
+
+def main_loop():
+    pass
+
+
+# Keep this at the bottom!
+keep_going = True
+while keep_going:
+    main_loop 
+
+""" 
+Recipe
+Recipe Manager
+main loop
+- save to file
+- load file
+- add recipe
+- view recipe
+- edit recipe
+category enum?
+
+
+
+ """
+
