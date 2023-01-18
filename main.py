4584f8d290159b3caf9ed47ca2e8e3ccdf05af69
Ellen Sunström Thörnberg <ellen.sunstrom@gmail.com>
2023-01-18 20:01:58 +0100

    Add recipe, show recipe list

--- main.py
+++ main.py
@@ -4,6 +4,30 @@ from recipeManager import *
 
 recipe_manager = RecipeManager()
 
+def add_recipe():
+    print("CREATE NEW RECIPE")
+    title = read_input("Title: ")
+    description = input("Description (optinal): ")
+    ingredients = read_input("Ingredients :")
+    instructions = read_input("Instructions: ")
+    new_recipe = Recipe(title, description, ingredients, instructions)
+    added = recipe_manager.add(new_recipe)
+    if added:
+        print("RECIPE ADDED")
+    else:
+        print ("UNABLE TO ADD RECIPE")
+
+def read_input(prompt):
+    while True:
+        user_input = input(prompt)
+        if user_input.strip() != "":
+            return user_input
+        print("Incorrect input. ")
+
+def show_recipe_list():
+    print(recipe_manager)
+    # TODO show submenu?
+
 def show_menu():
     print("What do you wish to do?")
     print("s) Show recipe list")
@@ -16,11 +40,11 @@ def main_loop():
     choice = input("Selection: ").strip().lower()
     match choice:
         case "s": # Show recipe list
-            pass
+            show_recipe_list()
         case "r": # Show recipe
             pass
         case "n": # Add recipe
-            pass
+            add_recipe()
         case "q": # Quit
             print("Bye!")
             keep_going = False  # TODO Why doesn't this work to break the loop?
--- recipeManager.py
+++ recipeManager.py
@@ -1,9 +1,29 @@
 
 class RecipeManager:
-
+    
     
     def __init__(self):
-       self.recipes = []
+       self.recipe_list = []
+    
+    def __str__(self):
+        if len(self.recipe_list) != 0:
+            str_list = ''
+            for recipe in self.recipe_list:
+                number = self.recipe_list(recipe)
+                str_list += "{number})  {recipe.title}"
+            str_list += ''
+            return str_list
+        return "The recipe collection is empty!"
 
+# TODO check list for title, do not add if title already present
+# TODO add in alphabetical order
     def add(self, recipe):
-        self.recipes.append(recipe)
+        self.recipe_list.append(recipe)
+        return True
+    
+    def get_recipe():
+        pass
+    
+    def remove_recipe(recipe):
+        return False
+    
